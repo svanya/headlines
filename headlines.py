0c13af434ff91fd5cce13d88d6971b84d513cee7
stefan <stefan@stefan.com>
2017-11-19 22:57:19 +0100

    Update with  config file

--- headlines.py
+++ headlines.py
@@ -1,30 +1,76 @@
+import config as sv
 import feedparser
+import json
+import time
+import urllib.request
 from flask import Flask
 from flask import render_template
 from flask import request
-import time
 
 app = Flask(__name__)
 
-RSS_FEEDS = {'kosmo': 'http://www.kosmonautix.cz/rubrika/micro/feed/',
-             'lupa': 'https://www.lupa.cz/rss/clanky/',
-             'zive': 'https://www.zive.cz/rss/sc-47/',
-             'root': 'https://www.root.cz/rss/zpravicky/'}
-
 
 @app.route("/")
-def get_news():
-    query = request.args.get("publication")
-    if not query or query.lower() not in RSS_FEEDS:
-        publication = "kosmo"
+def home():
+    gen_time = time.strftime("%d-%m-%Y %H:%M:%S")
+    # get customized headlines, based on user input or default
+    publication = request.args.get('publication')
+    if not publication:
+        publication = sv.DEFAULTS['publication']
+    articles = get_news(publication)
+    # get customized weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = sv.DEFAULTS['city']
+    weather = get_weather(city)
+    # get customized currency based on user input or default
+    currency_from = request.args.get("currency_from")
+    if not currency_from:
+        currency_from = sv.DEFAULTS['currency_from']
+    currency_to = request.args.get("currency_to")
+    if not currency_to:
+        currency_to = sv.DEFAULTS['currency_to']
+    rate = get_rate(currency_from, currency_to)
+    return render_template("home.html",
+                           articles=articles,
+                           weather=weather,
+                           currency_from=currency_from,
+                           currency_to=currency_to,
+                           rate=rate,
+                           gen_time=gen_time
+                           )
+
+
+def get_news(query):
+    if not query or query.lower() not in sv.RSS_FEEDS:
+        publication = sv.DEFAULTS['publication']
     else:
         publication = query.lower()
-    feed = feedparser.parse(RSS_FEEDS[publication])
-    gen_time = time.strftime("%d-%m-%Y %H:%M:%S")
-    return render_template("home.html",
-                           site=feed['feed'],
-                           gen_time=gen_time,
-                           articles=feed['entries'])
+    feed = feedparser.parse(sv.RSS_FEEDS[publication])
+    return feed['entries']
+
+
+def get_weather(query):
+    query = urllib.request.quote(query)
+    url = sv.WEATHER_URL.format(query)
+    data = urllib.request.urlopen(url).read().decode('utf8')
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description": parsed["weather"][0]["description"],
+                   "temperature": parsed["main"]["temp"],
+                   "city": parsed["name"],
+                   'country': parsed['sys']['country']
+                   }
+    return weather
+
+
+def get_rate(frm, to):
+    all_currency = urllib.request.urlopen(sv.CURRENCY_URL).read().decode('utf8')
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return to_rate/frm_rate
 
 
 if __name__ == '__main__':
